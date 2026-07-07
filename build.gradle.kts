8cccc71401904bf1fb6fd92dcf66457f159d4494
intisy (Finn Birich) <finn@birich.de>
2026-07-07 12:04:18 +0200

    chore(build): bump core to gh-v5.2.4.6; trim build-file comments

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ repositories {
 }
 
 dependencies {
-    githubCompileOnly("Slimefun5:Slimefun5:gh-v5.2.4.5")
+    githubCompileOnly("Slimefun5:Slimefun5:gh-v5.2.4.6")
     compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
 }
